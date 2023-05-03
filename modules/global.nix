7edd8d4169ec616fa82c92993a8c994b04c58a9a
ncn-ends <ncn949494@gmail.com>
2023-05-03 02:04:27 -0700

    global paths

--- modules/global.nix
+++ modules/global.nix
@@ -5,6 +5,9 @@
       name = "one";
       realName = "ncn";
     };
-    unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
+    basePath = {
+      linux = /etc/nixos; # TODO: incorporate these into the paths instead of relative paths
+      mac = ~/nix;
+    };
   };
 }
