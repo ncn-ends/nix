5135ff47b7ec0e84d10806f9a602e1fcf5397751
ncn-ends <ncn949494@gmail.com>
2023-06-16 08:35:23 -0700

    dotnet shell with working dotcover

--- shells/dotnet-shell.nix
+++ shells/dotnet-shell.nix
@@ -7,7 +7,12 @@ in mkShell {
   name = "dotnet-env";
   packages = [
     dotnet-sdk_6
-    jetbrains.rider
+    (jetbrains.rider.overrideAttrs (old: {
+      postPatch = old.postPatch + ''
+        interp="$(cat $NIX_CC/nix-support/dynamic-linker)"
+        patchelf --set-interpreter $interp plugins/dotCommon/DotFiles/linux-x64/JetBrains.Profiler.PdbServer
+      '';
+    }))
   ];
 
   shellHook = import ./shellHook.nix;
