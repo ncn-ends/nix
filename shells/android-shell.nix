badcff9a57b9e642bb96e4a6f121bb87a4cf08e4
ncn-ends <ncn949494@gmail.com>
2023-08-06 23:17:50 -0700

    attempt at android shell

--- shells/android-shell.nix
+++ shells/android-shell.nix
@@ -1,48 +1,12 @@
-# with import <nixpkgs> {};
-
-# let
-#   unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
-# in mkShell {
-#   name = "rn-env";
-#   packages = [
-#     nodejs
-#     nodePackages.npm
-#     nodePackages.yarn
-#     unstable.jetbrains.rider
-#     unstable.android-studio
-#     watchman
-#   ];
-
-#   shellHook = ''
-    
-
-#     echo "Mobile shell starting..."
-#   '';
-# }
-
-
-# todo: 
-#   - automate the creation of the repo directory. if it doesn't exist yet, clone it
-{ pkgs ? import <nixpkgs> {
-  config.android_sdk.accept_license = true;
-  config.allowUnfree = true;
-} }:
-
-with pkgs;
-
+with (import (fetchTarball https://github.com/nixos/nixpkgs/archive/nixpkgs-unstable.tar.gz) { config = { allowUnfree = true; }; });
 let
   android-nixpkgs = callPackage (import (builtins.fetchGit {
     url = "https://github.com/tadfisher/android-nixpkgs.git";
   })) {
-    # Default; can also choose "beta", "preview", or "canary".
     channel = "stable";
   };
   android-sdk = android-nixpkgs.sdk (sdkPkgs: with sdkPkgs; [
     cmdline-tools-latest
-    # build-tools-30-0-0
-    # platform-tools
-    # platforms-android-31
-    # emulator
   ]);
 in
 mkShell {
