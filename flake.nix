41c7173de9e4da296941b105b51ae9a55707dcee
ncn-ends <ncn949494@gmail.com>
2023-10-28 01:08:22 -0700

    split flakes at root

--- flake.nix
+++ flake.nix
@@ -3,9 +3,10 @@
     stable.url = "github:NixOS/nixpkgs/master";
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    devShellFlake.url = "path:./dev-shell.nix";
   };
 
-  outputs = {self, stable, unstable, flake-utils}:
+  outputs = {self, stable, unstable, devShellFlake, flake-utils}:
     let 
       eachDefaultSystem = flake-utils.lib.eachDefaultSystem;
 
@@ -13,49 +14,7 @@
         let 
           mkShell = stablePkgs.mkShell;
         in {
-          devShells.node = mkShell rec { 
-            name = "node-env";
-            
-            stablePackages = with stablePkgs; [
-              nodejs
-              nodePackages.npm
-              nodePackages.yarn
-              nodePackages.pnpm
-              nodePackages.javascript-typescript-langserver
-              nodePackages.typescript
-              ngrok
-            ];
-
-            unstablePackages = with unstablePkgs; [
-              jetbrains.rider
-              jetbrains.webstorm
-            ];
-
-            packages = stablePackages ++ unstablePackages;
-          };
-
-          devShells.dotnet = mkShell rec {
-            name = "dotnet-master-env";
-
-            stablePackages = with stablePkgs; [
-              (with dotnetCorePackages;combinePackages [
-                sdk_6_0
-                sdk_7_0
-              ])
-            ];
-
-            unstablePackages = with unstablePkgs; [
-              jetbrains.rider
-              # (jetbrains.rider.overrideAttrs (old: {
-              #   postPatch = old.postPatch + ''
-              #     interp="$(cat $NIX_CC/nix-support/dynamic-linker)"
-              #     patchelf --set-interpreter $interp plugins/dotCommon/DotFiles/linux-x64/JetBrains.Profiler.PdbServer
-              #   '';
-              # }))
-            ];
-
-            packages = stablePackages ++ unstablePackages;
-          };
+          devShells.node = devShellFlake.lib.nodeShell { inherit mkShell stablePkgs unstablePkgs; };
         };
 
     in eachDefaultSystem (system: defineShells {
