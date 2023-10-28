a71ac3783342acf074768edc5fe80460315c1d05
ncn-ends <ncn949494@gmail.com>
2023-10-27 23:51:48 -0700

    wip

--- dev-env-flakes/flake.nix
+++ dev-env-flakes/flake.nix
@@ -8,35 +8,40 @@
   outputs = {self, stable, unstable, flake-utils}:
     let 
       eachDefaultSystem = flake-utils.lib.eachDefaultSystem;
-      mkShell = stablePkgs.mkShell;
 
-      defineShells = { stablePkgs, unstablePkgs }: {
-        devShells.node = mkShell rec { 
-          name = "node-env";
-          
-          stablePackages = with stablePkgs; [
-            nodejs
-            nodePackages.npm
-            nodePackages.yarn
-            nodePackages.pnpm
-            nodePackages.javascript-typescript-langserver
-            nodePackages.typescript
-            ngrok
-          ];
+      defineShells = { stablePkgs, unstablePkgs }: 
+        let 
+          mkShell = stablePkgs.mkShell;
+        in {
+          devShells.node = mkShell rec { 
+            name = "node-env";
+            
+            stablePackages = with stablePkgs; [
+              nodejs
+              nodePackages.npm
+              nodePackages.yarn
+              nodePackages.pnpm
+              nodePackages.javascript-typescript-langserver
+              nodePackages.typescript
+              ngrok
+            ];
 
-          unstablePackages = with unstablePkgs; [
-            jetbrains.rider
-            jetbrains.webstorm
-          ];
+            unstablePackages = with unstablePkgs; [
+              jetbrains.rider
+              jetbrains.webstorm
+            ];
 
-          packages = stablePackages ++ unstablePackages;
-        };
+            packages = stablePackages ++ unstablePackages;
+          };
 
-        devShells.dotnet6 = mkShell rec {
+          devShells.dotnet6 = mkShell rec {
             name = "dotnet6-env";
 
-            packages = [
+            stablePackages = with stablePkgs; [
               dotnet-sdk_6
+            ];
+
+            unstablePackages = with unstablePkgs; [
               (jetbrains.rider.overrideAttrs (old: {
                 postPatch = old.postPatch + ''
                   interp="$(cat $NIX_CC/nix-support/dynamic-linker)"
@@ -44,8 +49,10 @@
                 '';
               }))
             ];
+
+            packages = stablePackages ++ unstablePackages;
+          };
         };
-      };
     in eachDefaultSystem (system: defineShells {
       stablePkgs = import stable { inherit system; config.allowUnfree = true; };
       unstablePkgs = import unstable { inherit system; config.allowUnfree = true; };
