51a370c6f7ee6af515fe336640a9e89df077106e
ncn-ends <ncn949494@gmail.com>
2023-11-07 01:03:17 -0800

    flake back

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    stable.url = "github:NixOS/nixpkgs/master";
+    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {self, stable, unstable, flake-utils}:
+    let 
+      # reuse
+      defaultSystem = "x86_64-linux";
+      stablePkgs = import stable { system = defaultSystem; config.allowUnfree = true; };
+      unstablePkgs = import unstable { system = defaultSystem; config.allowUnfree = true; };
+
+      # shells
+      mkDevShell = import ./dev-shells.nix;
+      mkShell = stablePkgs.mkShell;
+      passShellInputs = { inherit mkShell stablePkgs unstablePkgs; };
+
+      final = {
+        eval = 2 + 2;
+        devShells.${defaultSystem} = {
+          node = mkDevShell.nodeShell passShellInputs;
+          dotnet = mkDevShell.dotnetShell passShellInputs;
+        };
+      };
+
+    in final;
+}
