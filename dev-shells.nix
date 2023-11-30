f2d8b7d9d0b207e55846eed7608b26659ec7c1e8
ncn-ends <ncn949494@gmail.com>
2023-11-30 10:13:42 -0800

    copilot fix

--- dev-shells.nix
+++ dev-shells.nix
@@ -24,19 +24,21 @@
     name = "dotnet-master-env";
 
     stablePackages = with stable; [
-      (with dotnetCorePackages;combinePackages [
+      (with dotnetCorePackages; combinePackages [
         sdk_6_0
         sdk_7_0
       ])
     ];
 
     unstablePackages = with unstable; [
-      (jetbrains.rider.overrideAttrs (old: {
+      # rider, with dotCover fix and copilot plugin
+      # - copilot won't work by installing through GUI
+      (jetbrains.plugins.addPlugins (jetbrains.rider.overrideAttrs (old: {
         postPatch = old.postPatch + ''
           interp="$(cat $NIX_CC/nix-support/dynamic-linker)"
           patchelf --set-interpreter $interp plugins/dotCommon/DotFiles/linux-x64/JetBrains.Profiler.PdbServer
         '';
-      }))
+      })) [ "github-copilot" ])
     ];
 
     packages = stablePackages ++ unstablePackages;
@@ -54,7 +56,11 @@
     ];
 
     unstablePackages = with unstable; [
-      (jetbrains.rider.overrideAttrs (old: rec {
+      # rider EAP, with dotCover fix and copilot plugin
+      # - copilot won't work by installing through GUI
+      # - .NET 8 features aren't available on non-EAP yet
+
+      (jetbrains.plugins.addPlugins (jetbrains.rider.overrideAttrs (old: rec {
         # https://download.jetbrains.com/rider/JetBrains.Rider-2023.3-EAP8-233.11799.49.Checked.tar.gz
         version = "2023.3-EAP8-233.11799.49.Checked";
         name = "rider-${version}";
@@ -66,7 +72,7 @@
           interp="$(cat $NIX_CC/nix-support/dynamic-linker)"
           patchelf --set-interpreter $interp plugins/dotCommon/DotFiles/linux-x64/JetBrains.Profiler.PdbServer
         '';
-      }))
+      })) [ "github-copilot" ])
     ];
 
     packages = stablePackages ++ unstablePackages;
