2ae2e0e609f517bea2174279dcaafaf1a9dd1573
ncn-ends <ncn.ends@gmail.com>
2023-12-02 16:36:19 -0800

    some nix darwin configs and cleaned up dep files

--- modules/foundation.mac.nix
+++ modules/foundation.mac.nix
@@ -10,11 +10,68 @@
   services.nix-daemon.enable = true;
   nixpkgs.config.allowUnfree = true;
 
+  # these might be needed when adding zsh to nix, but they could also cause problems
+  # environment.shells = [ pkgs.bash pkgs.zsh ];
+  # environment.loginShell = pkgs.zsh;
+
+  # may be needed when adding homebrew to nix config
+  # environment.systemPath = [ "/opt/homebrew/bin" ];
+  
+  # not sure what this is for
+  # environment.pathsToLink = [ "/Applications" ];
+
+  # example homebrew config
+    #   homebrew = {
+    #   enable = true;
+    #   caskArgs.no_quarantine = true;
+    #   global.brewfile = true;
+    #   masApps = {
+    #     Xcode = 497799835;
+    #   };
+    #   casks = [];
+    # };
+
+  # i think this is if you want to customize key remapping within nix
+  # system.keyboard.enableKeyMapping = true; 
   system.defaults = {
-    # always show file extensions in finder
+    # dark mode
+    NSGlobalDomain.AppleInterfaceStyle = "Dark";
+    # always show file extensions in finder and in general
     finder.AppleShowAllExtensions = true;
+    NSGlobalDomain.AppleShowAllExtensions = true;
     # show full path at the top of the finder window
     finder._FXShowPosixPathInTitle = true;
+    # show breadcrumbs in finder
+    finder.ShowPathbar = true;
+    # status bar with stats at bottom of finder
+    finder.ShowStatusBar = true;
+    # default to list view in finder
+    finder.FXPreferredViewStyle = "Nlsv";
+    # show hidden files in finder and in general
+    finder.AppleShowAllFiles = true; 
+    NSGlobalDomain.AppleShowAllFiles = true;
+    # size of icons in dock
+    dock.tilesize = 36;
+    # only shows open apps in the dock
+    dock.static-only = true; 
+    # don't automatically rearrange spaces based on most recent use. who thought this was a good idea? 
+    dock.mru-spaces = false; 
+    # time to hide dock, default is 1.0 
+    dock.autohide-time-modifier = 0.5;
+    dock.autohide = true;
+    # don't save documents to iCloud by default
+    NSGlobalDomain.NSDocumentSaveNewDocumentsToCloud = false;
+    # no automatic spelling correction
+    NSGlobalDomain.NSAutomaticSpellingCorrectionEnabled = false;
+    # always show all scrollbars
+    NSGlobalDomain.AppleShowScrollBars = "Always";
+    # how long to wait before repeating character when held down
+    NSGlobalDomain.InitialKeyRepeat = 14;
+    # speed of character repeat when held down
+    NSGlobalDomain.KeyRepeat = 1;
+
+    # NSGlobalDomain.com.apple.trackpad.scaling = 1.2; # trackpad tracking speed, default is 1 
+    # NSGlobalDomain.com.apple.keyboard.fnState = false; # f1-f12 keys as function keys
   };
 
 
