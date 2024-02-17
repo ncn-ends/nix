2f47d6ef2d881b040814d71c9fea3497eff89e0e
ncn-ends <ncn949494@gmail.com>
2024-02-17 15:39:35 -0800

    serve vaultwarden from tailscale url

--- modules/server.caddy.nix
+++ modules/server.caddy.nix
@@ -1,11 +1,16 @@
 { stable, config, ...}: {
-
+  environment.systemPackages = [stable.caddy];
   services.caddy = {
     enable = true;
 
     virtualHosts."nixos.tail7d98c.ts.net".extraConfig = ''
-      respond "Hello, world!"
+      reverse_proxy localhost:8222
     '';
   };
-
 }
+# https://github.com/NixOS/nixpkgs/blob/nixos-23.11/nixos/modules/services/security/vaultwarden/default.nix
+    # virtualHosts."nixos.tail7d98c.ts.net/vw".extraConfig = ''
+    # '';
+    # virtualHosts."http://hub.ncn.dev".extraConfig = ''
+    #   respond "Hello, world!"
+    # '';
--- modules/server.vaultwarden.nix
+++ modules/server.vaultwarden.nix
@@ -1,4 +1,19 @@
 { stable, config, ...}:
-{
-  # services.vaultwarden.enable = true;
+let
+  vwDir = "/run/media/one/shape/vaultwarden";
+in {
+  # services.vaultwarden = {
+  #   enable = true;
+  #   # backupDir = "${vwDir}/backup";
+  #   config = {
+  #     DATA_FOLDER = vwDir;
+  #   };
+  # };
+
+  # systemd.tmpfiles.rules = [
+  #   "d ${vwDir} 0755 vaultwarden vaultwarden - -"
+  #   # "d ${vwDir}/backup 0755 vaultwarden vaultwarden - -"
+  # ];
+
+  services.vaultwarden.enable = true;
 }
