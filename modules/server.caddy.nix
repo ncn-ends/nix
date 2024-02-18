970ad40a06745b95c1134f4fb9094ea8cff23189
ncn-ends <ncn949494@gmail.com>
2024-02-17 16:32:40 -0800

    updating caddy route for vaultwarden

--- modules/server.caddy.nix
+++ modules/server.caddy.nix
@@ -4,13 +4,9 @@
     enable = true;
 
     virtualHosts."nixos.tail7d98c.ts.net".extraConfig = ''
-      reverse_proxy localhost:8222
+      handle_path /vw/* {
+        reverse_proxy localhost:8222
+      }
     '';
   };
 }
-# https://github.com/NixOS/nixpkgs/blob/nixos-23.11/nixos/modules/services/security/vaultwarden/default.nix
-    # virtualHosts."nixos.tail7d98c.ts.net/vw".extraConfig = ''
-    # '';
-    # virtualHosts."http://hub.ncn.dev".extraConfig = ''
-    #   respond "Hello, world!"
-    # '';
