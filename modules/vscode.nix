50eff10cce36f452eb6943e3a32296f6d5bb692a
ncn-ends <ncn949494@gmail.com>
2024-12-27 07:16:46 -0800

    whole bunch of vs code improvements, and nix support

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -1,11 +1,20 @@
-
 { imports, config, services, machine, ... }:
+let
+  stable = imports.stable;
+in
 {
+  environment.systemPackages = [
+    # these 3 are for nix support in vs code
+    stable.nil
+    stable.nixpkgs-fmt
+    stable.statix
+  ];
+
   home-manager.users.${machine.user} = { ... }: {
     programs.vscode = {
       enable = true;
       mutableExtensionsDir = false; # home-manager stopped installing extensions at some point and this fixed it
-      extensions = with imports.stable.vscode-extensions; [
+      extensions = with stable.vscode-extensions; [
         vscodevim.vim
         bbenoist.nix
         jnoortheen.nix-ide
@@ -13,7 +22,7 @@
         formulahendry.auto-close-tag
         dbaeumer.vscode-eslint
         eamodio.gitlens
-        esbenp.prettier-vscode 
+        esbenp.prettier-vscode
         # kamadorueda.alejandra
         # dlasagno.rasi
         tomoki1207.pdf
@@ -30,8 +39,18 @@
         "workbench.tree.indent" = 20;
         "emmet.showExpandedAbbreviation" = "never"; # emmet gets in the way
 
+        # visual guide for line length
+        "editor.rulers" = [ 80 120 ];
+
+        # see bracket pairs easier
+        "editor.bracketPairColorization.enabled" = true;
+        "editor.guides.bracketPairs" = true;
+
         # when opening vs code won't open last closed window, opens blank window
         "window.restoreWindows" = "none";
+
+        # don't open a new tab with release notes when it updates
+        "update.showReleaseNotes" = false;
 
         # don't open modal on goto, just go to
         "editor.gotoLocation.multipleTypeDefinitions" = "goto";
@@ -41,22 +60,24 @@
         "editor.wordWrap" = "on";
         "editor.wrappingColumn" = 0;
         "editor.cursorBlinking" = "smooth";
-        "editor.unicodeHighlight.invisibleCharacters" =  true;
+        "editor.unicodeHighlight.invisibleCharacters" = true;
         "editor.menuBarVisibility" = "hidden";
         "editor.formatOnPaste" = true;
         "breadcrumbs.enabled" = false;
         "customizeUI.activityBar" = "top";
-        "files.autoSave"= "afterDelay"; 
-        "files.autoSaveDelay"= 1000;
+        "files.autoSave" = "afterDelay";
+        "files.autoSaveDelay" = 1000;
+        # improves folder structure visibility
+        "explorer.compactFolders" = true;
 
+        # vim
         "vim.normalModeKeyBindings" = [
           {
-            "before" = ["g" "r"];
-            "commands" = ["editor.action.goToReferences"];
+            "before" = [ "g" "r" ];
+            "commands" = [ "editor.action.goToReferences" ];
           }
         ];
         "vim.showMarksInGutter" = true;
-
         "vim.handleKeys" = {
           "<C-f>" = false;
           "<C-x>" = false;
@@ -65,7 +86,9 @@
           "<C-p>" = false;
           "<C-w>" = false;
         };
+        "vim.hlsearch" = true; # highlight search matches
 
+        # js / ts
         "[typescriptreact]" = {
           "editor.defaultFormatter" = "esbenp.prettier-vscode";
         };
@@ -74,6 +97,19 @@
         };
         "[javascript]" = {
           "editor.defaultFormatter" = "esbenp.prettier-vscode";
+        };
+        "javascript.updateImportsOnFileMove.enabled" = "always";
+        "typescript.updateImportsOnFileMove.enabled" = "always";
+
+        # nix
+        "nix.enableLanguageServer" = true;
+        "nix.serverPath" = "nil";
+        "nix.serverSettings" = {
+          "nil" = {
+            "formatting" = {
+              "command" = "nixpkgs-fmt";
+            };
+          };
         };
       };
     };
